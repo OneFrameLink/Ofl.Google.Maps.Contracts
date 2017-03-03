93d123edc7b0ebbf6b33ba45986225d26b144e36
casperOne <casperOne@caspershouse.com>
2017-03-03 09:46:57 -0500

    Initial commit.

---
+++ Ofl.Google.Maps.Contracts/Serialization/Newtonsoft/SecondsToTimeSpanJsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Ofl.Google.Maps.Serialization.Newtonsoft
+{
+    internal class SecondsToTimeSpanJsonConverter : JsonConverter
+    {
+        #region Overrides of JsonConverter
+
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            // Not implemented.
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Validate parameters.
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+
+            // Token type must be an integer.
+            if (reader.TokenType != JsonToken.Integer)
+                throw new InvalidOperationException($"Unexpected token parsing date. Expected Integer, got {reader.TokenType}.");
+
+            // Convert.
+            return TimeSpan.FromSeconds((long) reader.Value);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Ofl.Google.Maps.Contracts/TimeZoneRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ofl.Google.Maps
+{
+    public class TimeZoneRequest
+    {
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
