002d005975974359639696e0e1084928e2de4e98
casperOne <casperOne@caspershouse.com>
2017-03-27 08:19:14 -0400

    Added Geocoding call.

--- Ofl.Google.Maps.Contracts/IMapsClient.cs
+++ Ofl.Google.Maps.Contracts/IMapsClient.cs
@@ -1,10 +1,14 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Ofl.Google.Maps.Geocode;
+using Ofl.Google.Maps.TimeZone;
 
 namespace Ofl.Google.Maps
 {
     public interface IMapsClient
     {
         Task<TimeZoneResponse> GetTimeZoneAsync(TimeZoneRequest request, CancellationToken cancellationToken);
+
+        Task<GeocodeResponse> GeocodeAsync(GeocodeRequest request, CancellationToken cancellationToken);
     }
 }
